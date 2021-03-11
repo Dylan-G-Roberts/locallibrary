d682032fdc7ad1f29ae6357b82b138fd3a98cb5d
Dylan Roberts <dylangardnerroberts@gmail.com>
2021-03-10 23:47:12 -0400

    Added configuration of list views for admins

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,9 +1,25 @@
 from django.contrib import admin
 from .models import Author, Genre, Book, BookInstance
 
-admin.site.register(Book)
-admin.site.register(Author)
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
+
+# admin.site.register(Author, AuthorAdmin)
+# admin.site.register(Book)
+# admin.site.register(BookInstance)
+
+
 admin.site.register(Genre)
-admin.site.register(BookInstance)
+
 
 
